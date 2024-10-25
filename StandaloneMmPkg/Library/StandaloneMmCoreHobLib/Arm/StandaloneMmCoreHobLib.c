551911dd28c314574e069fed023018806ae3798f
Kenneth Lautner <klautner@microsoft.com>
2024-10-25 21:43:06 +0000

    Merged PR 8124: Remove security fix comments that were upstreamed
    Remove security fix comments because the original fix was upstreamed making them unnecessary.
    
    ----
    #### AI description  (iteration 1)
    #### PR Classification
    Code cleanup
    
    #### PR Summary
    This pull request removes outdated security fix comments that have been upstreamed.
    - Removed Microsoft Corporation copyright and associated security fix comments from `StandaloneMmPkg/Library/StandaloneMmCoreHobLib/Arm/StandaloneMmCoreHobLib.c`
    - Removed Microsoft Corporation copyright and associated security fix comments from `StandaloneMmPkg/Library/StandaloneMmHobLib/StandaloneMmHobLib.c`

--- StandaloneMmPkg/Library/StandaloneMmCoreHobLib/Arm/StandaloneMmCoreHobLib.c
+++ StandaloneMmPkg/Library/StandaloneMmCoreHobLib/Arm/StandaloneMmCoreHobLib.c
@@ -3,7 +3,6 @@
 
 Copyright (c) 2006 - 2014, Intel Corporation. All rights reserved.<BR>
 Copyright (c) 2017 - 2018, ARM Limited. All rights reserved.<BR>
-Copyright (c) Microsoft Corporation.<BR> // MU_SEC_TCBZ4166 -  Mitigate integer overflow
 
 SPDX-License-Identifier: BSD-2-Clause-Patent
 
